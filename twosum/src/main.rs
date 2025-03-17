27e8e738d21e02e03d3574a9e879bce234c76d7f
Paulo Moreira <pdevsloth3@gmail.com>
2025-03-16 21:26:52 -0400

    Removed accidentally added debug files + added gitignore

--- twosum/src/main.rs
+++ twosum/src/main.rs
@@ -5,12 +5,12 @@ impl Solution {
         // [0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], and [2,2] and see if the values of these added together
         // equal the target.
         //Really what we need are unordered pairs of possible indices, add them to a list and find which  one has the target.
-        //
+        //We can reduce the number of pairs produced by removing the ones where the index is the same e.g. nums[0] with nums[0]
+        //We can also reduce the number of pairs produced by removing the ones where they are duplicated, e.g. (0, 1) and (1, 0) are the same
         let ordered_indices = generate_ordered_pairs(nums.len() - 1);
 
         for indices in ordered_indices.iter() {
             let (index1, index2) = indices;
-            println!("{}", index1);
 
             if nums[*index1] + nums[*index2] == target && *index1 != *index2 {
                 found_indices.push(*index1 as i32);
@@ -27,7 +27,7 @@ fn generate_ordered_pairs(n: usize) -> Vec<(usize, usize)> {
     let mut pairs = Vec::new();
 
     for i in 0..=n {
-        for j in 0..=n {
+        for j in (i + 1)..=n {
             pairs.push((i, j));
         }
     }
--- twosum/target/debug/deps/twosum-5f4955158936d29d.d
+++
@@ -1,5 +0,0 @@
-C:\CodingProjects\l33tcode\twosum\target\debug\deps\libtwosum-5f4955158936d29d.rmeta: src\main.rs
-
-C:\CodingProjects\l33tcode\twosum\target\debug\deps\twosum-5f4955158936d29d.d: src\main.rs
-
-src\main.rs:
